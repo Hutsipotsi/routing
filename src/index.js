77054a926c5cbcd51e3e7a4a4fe6e846bcf05b9f
Hutsipotsi <93660933+Hutsipotsi@users.noreply.github.com>
2022-01-26 21:17:53 +0200

    Valmis

--- src/index.js
+++ src/index.js
@@ -12,12 +12,12 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-ReactDOM.render(
+/* ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
-);
+);*/
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
