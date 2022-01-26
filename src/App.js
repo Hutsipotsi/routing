af1c41cdabce0a33f3f76502d1db3ed60554e471
Hutsipotsi <93660933+Hutsipotsi@users.noreply.github.com>
2022-01-26 21:26:49 +0200

    korjauksia

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React from "react";
-import { Switch, Route } from "react-router-dom";
-import Home from "./Home";
-import About from "./About";
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Home from './Home';
+import About from './About';
 
 function App() {
   return (
